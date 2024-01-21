5dd643e66d23de2515a3420658cf9391c008d253
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2024-01-21 22:00:57 +0300

    Main page design is implemented

--- frontend/src/components/ProcessVideoSettings/ProcessVideoSettings.jsx
+++ frontend/src/components/ProcessVideoSettings/ProcessVideoSettings.jsx
@@ -3,7 +3,17 @@ import classes from "./ProcessVideoSettings.module.css"
 const ProcessVideoSettings = () => {
     return (
         <div className={classes.ProcessVideoSettings}>
-
+            <div style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                <text style={{fontSize: 32, fontFamily: "Inter"}}>FPS</text>
+                <select id="fps" style={{padding: "5%", width: "72px", height: "48px", fontSize: "24px"}}>
+                    <option value={16}>16</option>
+                    <option value={32}>32</option>
+                    <option value={48}>48</option>
+                    <option value={64}>64</option>
+                </select>
+            </div>
+            <div style={{height: "10%"}}></div>
+            <button className={classes.ProcessVideoButton}>Process</button>
         </div>
     )
 }
--- frontend/src/components/ProcessedVideoColumn/ProcessedVideoColumn.jsx
+++ frontend/src/components/ProcessedVideoColumn/ProcessedVideoColumn.jsx
@@ -1,9 +1,20 @@
 import classes from "./ProcessedVideoColumn.module.css"
+import amongUsPlaceholder from "../../img/YellowAmongUsCharacterPNGImage.png"
 
 const ProcessedVideoColumn = () => {
     return (
         <div className={classes.ProcessedVideoColumn}>
-
+                <div style={{
+                    height: "80%",
+                    width: "100%",
+                    borderStyle: "solid",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column"
+                }}>
+                    <img src={amongUsPlaceholder} style={{ width: "50%"}} />
+            </div>
         </div>
     )
 }
